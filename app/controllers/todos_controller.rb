0ce6f12051773f5143d73f1b58c38ae786397cc7
Deanna Garriga <deanna@dnarix.com>
2017-09-14 13:03:50 +0000

    Initial commit

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,53 @@
+class TodosController < ApplicationController
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save 
+      flash[:notice] = "Todo was created succesfully!"
+     redirect_to todo_path(@todo)
+    else
+      render "new"
+    end
+  end
+  
+  def show
+    @todo = Todo.find(params[:id])
+  end
+  
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+  
+  def update
+    @todo = Todo.find(params[:id])
+    if @todo.update(todo_params)
+      flash[:notice] = "Todo was succesfully updated."
+      redirect_to todo_path(@todo)
+    else
+      render "edit"
+    end
+  end
+  
+  def index
+    @todos = Todo.all
+  end
+  
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    flash[:notice] = "Todo was deleted."
+    redirect_to todos_path
+  end
+  
+  private
+  
+    def todo_params
+      params.require(:todo).permit(:name, :description)
+    end
+  
+  
+end
